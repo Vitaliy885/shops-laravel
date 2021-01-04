a12c667d0e37d679c01ec96765df2bdfba189334
frontend <front@lara.com>
2021-01-04 17:01:13 +0200

    footer

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,4 +1,4 @@
-<header class="header container">
+<header class="header">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="{{asset('/')}}">Logo</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
