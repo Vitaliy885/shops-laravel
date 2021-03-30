0699a4d47254af0d943f30093b6485ffc9a4aeb3
Vitalik885 <Vitalik885@gmail.com>
2021-03-30 21:13:14 +0300

    fix

--- resources/views/pages/category.blade.php
+++ resources/views/pages/category.blade.php
@@ -40,60 +40,21 @@
                     </nav>
                     <!--End Nav Button  -->
                 </div>
-                <!-- Select items -->
-                <div class="select-this">
-                    <form action="#">
-                        <div class="select-itms">
-                            <select name="select" id="select1">
-                                <option value="">40 per page</option>
-                                <option value="">50 per page</option>
-                                <option value="">60 per page</option>
-                                <option value="">70 per page</option>
-                            </select>
-                        </div>
-                    </form>
-                </div>
             </div>
             <!-- Nav Card -->
             <div class="tab-content" id="nav-tabContent">
                 <!-- card one -->
                 <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
                     <div class="row">
-                        <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6">
-                            <a href="{{ asset('/product') }}" class="single-popular-items mb-50 text-center">
-                                <div class="popular-img">
-                                    <img src="images/gallery/popular1.png" alt="">
-                                    <div class="img-cap">
-                                        <span>Add to cart</span>
-                                    </div>
-                                    <div class="favorit-items">
-                                        <span class="flaticon-heart"></span>
-                                    </div>
-                                </div>
-                                <div class="popular-caption">
-                                    <h3><a href="product_details.html">Thermo Ball Etip Gloves</a></h3>
-                                    <span>$ 45,743</span>
-                                </div>
-                            <a/>
-                        </div>
-                        <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6">
-                            <a href="{{ asset('/product') }}" class="single-popular-items mb-50 text-center">
-                                <div class="popular-img">
-                                    <img src="images/gallery/popular2.png" alt="">
-                                    <div class="img-cap">
-                                        <span>Add to cart</span>
-                                    </div>
-                                    <div class="favorit-items">
-                                        <span class="flaticon-heart"></span>
-                                    </div>
-                                </div>
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+                            <a href="{{ asset('/product') }}" style="background-image: url('{{ asset('images/category/sectin_bg1.png') }}');" class="single-popular-items mb-50 text-center">
                                 <div class="popular-caption">
                                     <h3><a href="product_details.html">Thermo Ball Etip Gloves</a></h3>
                                     <span>$ 45,743</span>
                                 </div>
                             </a>
                         </div>
-                        <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6">
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
                             <a href="{{ asset('/product') }}" class="single-popular-items mb-50 text-center">
                                 <div class="popular-img">
                                     <img src="images/gallery/popular3.png" alt="">
@@ -110,7 +71,7 @@
                                 </div>
                             </a>
                         </div>
-                        <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6">
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
                             <div class="single-popular-items mb-50 text-center">
                                 <div class="popular-img">
                                     <img src="images/gallery/popular4.png" alt="">
@@ -127,7 +88,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6">
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
                             <div class="single-popular-items mb-50 text-center">
                                 <div class="popular-img">
                                     <img src="images/gallery/popular5.png" alt="">
@@ -144,7 +105,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6">
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
                             <div class="single-popular-items mb-50 text-center">
                                 <div class="popular-img">
                                     <img src="images/gallery/popular6.png" alt="">
